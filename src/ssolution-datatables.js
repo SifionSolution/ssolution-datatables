a5fb7c666bb6b78827eded235a849d62a7d77680
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2014-12-09 16:21:12 -0200

    Adding jQuery plugin boilerplate

--- src/ssolution-datatables.js
+++ src/ssolution-datatables.js
@@ -1,3 +1,29 @@
 ;(function($, window, document, undefined) {
-	
+	$.SSolutionTable = function(element, options) {
+		var plugin = this,
+			$element = $(element),
+			defaults = {
+			};
+
+		plugin.settings = {};
+
+		plugin.init = function() {
+			plugin.settings = $.extend(true, {}, defaults, options);
+		};
+
+		plugin.foo_public_method = function() {
+		};
+
+		var foo_private_method = function() {
+		};
+
+		plugin.init();
+	};
+
+	$.fn.ssolutionTable = function(options) {
+		return this.each(function() {
+			if (undefined == $(this).data('plugin_ssolutionTable'))
+				$(this).data('plugin_ssolutionTable', new $.SSolutionTable(this, options));
+		});
+	};
 })(jQuery, window, document);
